7bb7dae2eeb5dd0ddf22516591755d13cab91c80
alex23ptr <alex23ptr@gmail.com>
2018-04-23 22:03:18 +0300

    ap slider

--- AP/customSlider/controllers/IndexController.php
+++ AP/customSlider/controllers/IndexController.php
@@ -3,7 +3,22 @@ class AP_customSlider_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo "Hello from module";	 
+		
+		$this->loadLayout();
+		$this->getLayout()->getBlock("head")->setTitle($this->__("Slider page"));
+	    $breadcrumbs = $this->getLayout()->getBlock("breadcrumbs");
+		$breadcrumbs->addCrumb("home", array(
+					"label" => $this->__("Home Page"),
+					"title" => $this->__("Home Page"),
+					"link"  => Mage::getBaseUrl()
+			   ));
+
+		$breadcrumbs->addCrumb("customslider", array(
+					"label" => $this->__("custom slider"),
+					"title" => $this->__("custom slider")
+			   ));
+		$this->renderLayout();
+        //echo "Hello from module";	 
 
         
     }
